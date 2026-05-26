964cbb605e9611bb15a12354042e5bd19018bfbf
Zayn Ashraf <zaynashraf123@gmail.com>
2026-05-25 17:43:00 -0700

    Expose & Explore

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab8-Starter
+
+## Lab Partners: N/A
+
+## Graceful Degradation and Service Workers
+
+Graceful degradation is the practice of building a web app so that it still provides core functionality even when certain features are unavailable. Service workers are a useful tool when it comes to achieving this. They can intercept network requests and serve cached responses. This makes it so that the app still retains its main functionality even when the user is experiencing poor connection. Service workers allow the app to actually work and not completely crash when users lose network access. 
--- sw.js
+++ sw.js
@@ -7,9 +7,21 @@ const CACHE_NAME = 'lab-8-starter';
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
-      //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are                                                          
+      //            added to the cache when the ServiceWorker is installed 
+      return cache.addAll([
+        './',
+        './index.html',
+        './assets/scripts/main.js',
+        './assets/scripts/RecipeCard.js',
+        './assets/styles/main.css',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+      ]);
     })
   );
 });
@@ -32,9 +44,22 @@ self.addEventListener('fetch', function (event) {
   //       fetch(event.request)
   // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
   /*******************************/
-  // B7. TODO - Respond to the event by opening the cache using the name we gave
-  //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  // B7. TODO - Respond to the event by opening the cache using the name we gave                                                        
+  //            above (CACHE_NAME) 
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      // B8. TODO - If the request is in the cache, return with the cached version.                                                         
+      //            Otherwise fetch the resource, add it to the cache, and return                                                         
+      //            network response.  
+      return cache.match(event.request).then(function (cachedResponse) {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request).then(function (networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
 });
